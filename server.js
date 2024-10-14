97cd0512a41bc821609b8e876d7e6eb7c850b0db
sangeethvp <sangeethvp2002@gmail.com>
2024-10-15 00:41:02 +0530

    added the changes

--- server.js
+++ server.js
@@ -19,5 +19,6 @@ app.use('/api/recipes',recipeRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
-    console.log("server is running in https://localhost:5000 ")
+    console.log("server is running in https://localhost:5000/api/recipes ")
 })
+
